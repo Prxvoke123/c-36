b410477392e4cd46edfd9707b7e666bdb3f3e8b0
Prxvoke123 <72167000+Prxvoke123@users.noreply.github.com>
2020-11-20 16:50:49 -0500

    Add files via upload

---
+++ js/player.js
@@ -0,0 +1,21 @@
+class Player{
+    constructor(){}
+
+    getCount(){
+        database.ref("playerCount").on("value",function(data){
+            playerCount = data.val()
+        })
+    }
+
+    updateCount(count){
+        database.ref("/").update({
+            playerCount:count
+        })
+    }
+
+    update(name){
+        database.ref("player"+playerCount).set({
+            name:name
+        })
+    }
+}
